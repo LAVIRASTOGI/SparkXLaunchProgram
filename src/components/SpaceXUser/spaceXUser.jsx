7d6f52f3cc41ffb3acda4b0db4b3586064ceed1f
Lavi Rastogi <1993lavirastogi@gmail.com>
2020-09-05 22:09:57 +0530

    dymanic value of userlaunch program

--- src/components/SpaceXUser/spaceXUser.jsx
+++ src/components/SpaceXUser/spaceXUser.jsx
@@ -8,7 +8,7 @@ import { connect } from "react-redux";
 
 function mapStateToProps(state) {
   return {
-    userdata: state.userSpaceXdata.userList,
+    userdata: state.userSpaceXdata,
   };
 }
 
@@ -17,82 +17,28 @@ function mapDispatchToProps(dispatch) {
 }
 
 class SpaceXUserList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userlist: [
-        {
-          image: "https://images2.imgbox.com/40/e3/GypSkayF_o.png",
-          "flight_number":1,
-      "mission_name":"FalconSat",
-          missionId:  ["EE86F74"],
-          launch_year: "2006",
-          launch_success: true,
-          land_success: true,
-        },
-        {
-          image: "https://images2.imgbox.com/40/e3/GypSkayF_o.png",
-          flight_number:1,
-        mission_name:"FalconSat",
-          missionId: ["EE86F74","EE86F74"],
-          launch_year: "2006",
-          launch_success: false,
-          land_success: null,
-        },
-        {
-          image: "https://images2.imgbox.com/40/e3/GypSkayF_o.png",
-          flight_number:1,
-          mission_name:"FalconSat",
-          missionId: [],
-          launch_year: "2006",
-          launch_success: false,
-          land_success: null,
-        },
-        {
-          image: "https://images2.imgbox.com/40/e3/GypSkayF_o.png",
-          flight_number:1,
-          mission_name:"FalconSat",
-          missionId: [],
-          launch_year: "2006",
-          launch_success: false,
-          land_success: null,
-        },
-        {
-          image: "https://images2.imgbox.com/40/e3/GypSkayF_o.png",
-          flight_number:1,
-          mission_name:"FalconSat",
-          missionId: [],
-          launch_year: "2006",
-          launch_success: false,
-          land_success: null,
-        },
-      ],
-    };
-  }
-
   componentDidMount() {
-    var filterdata = { landing: "", launch: "", year: "" };
+    let filterdata = { landing: "", launch: "", year: "" };
     this.props.getUserListAction(filterdata);
   }
 
   render() {
-    var x=this.state.userlist.map((ele,index)=>{
-      return (
-        <Col xs={12} md={6} lg={3}>
-         <CardLauncher key={index} userData={ele} ></CardLauncher>
-        </Col>
-       
-      )
-     
-    })
+    let userValue = this.props.userdata.userList;
+    let lanchList = null;
+    if (userValue.length > 0) {
+      lanchList = userValue.map((ele, index) => {
+        return (
+          <Col xs={12} md={6} lg={3}>
+            <CardLauncher key={index} userData={ele}></CardLauncher>
+          </Col>
+        );
+      });
+    }
     return (
       <>
-      <Container  >
-        <Row>
-        {x} 
-        </Row>
-      </Container>
-      
+        <Container>
+          <Row>{lanchList}</Row>
+        </Container>
       </>
     );
   }
